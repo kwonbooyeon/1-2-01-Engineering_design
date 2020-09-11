21b0bd74ad35587398427ff349a5e9c45dcc2d2e
kwonbuyeon <qndusrnjs@gmail.com>
2020-09-11 17:54:29 +0900

    주석추가 강의4 실습 LED 점멸제어 테스트(예제3)

--- 04_example_3/04_example_3.ino
+++ 04_example_3/04_example_3.ino
@@ -1,26 +1,24 @@
 #define PIN_LED 13
 unsigned int count, toggle;
 void setup() {
-  // put your setup code here, to run once:
     pinMode(PIN_LED, OUTPUT);
-    Serial.begin(115200);
+    Serial.begin(115200);//Initialize serial port
     while(!Serial){
-      ;
+      ;//wait for serial port to connect.
       }//초기화
     Serial.println("HelloWorld!");
     count=toggle=0;
-    digitalWrite(PIN_LED, toggle);
+    digitalWrite(PIN_LED, toggle);// turn off LED
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
     Serial.println(++count);
-    toggle = toggle_state(toggle); 
-    digitalWrite(PIN_LED, toggle);
-    delay(1000);
+    toggle = toggle_state(toggle); // toggle LED value
+    digitalWrite(PIN_LED, toggle); // update LED status
+    delay(1000); // wait for 1,000 milliseconds
 }
 
 int toggle_state(int toggle) {
-    toggle = count%2;
-    return toggle;
+    toggle = count%2; //count의 홀 짝 기준으로 0,1 반복 출력
+    return toggle ;
   }
