17472411229d2b195c2c0eab6069eb322bac742e
kwonbuyeon <qndusrnjs@gmail.com>
2020-09-14 10:42:43 +0900

    강의5(3-1주차) GPIO를 통한 LED제어(실습2)

---
+++ 05_example_02/05_example_02.ino
@@ -0,0 +1,27 @@
+unsigned int count, toggle, PIN_LED;
+
+void setup() {
+  // put your setup code here, to run once:
+    PIN_LED=7;
+    count=toggle=0;
+    pinMode(PIN_LED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    ++count; // 초 세기
+    toggle = toggle_state(toggle); // toggle LED value
+    digitalWrite(PIN_LED, toggle); // update LED status
+    delay(100); // wait for 100 millisecond (깜빡임 때문에)
+}
+
+int toggle_state(int toggle) {
+    if (count%20<10) {
+      toggle = 1 ; //카운트의 초반 10회(1초)간  불을 켜둠
+      }
+    else{
+      toggle = count%2;//count의 홀 짝 기준으로 0,1 반복 출력으로 깜빡임
+    }
+    
+    return toggle ;
+  }
